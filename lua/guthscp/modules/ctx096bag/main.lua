17c1cb1461542fdccd8fbbf748bbefe062c41c7c
Certurix <46199479+Certurix@users.noreply.github.com>
2022-08-16 16:55:20 +0200

    Add files via upload

---
+++ lua/guthscp/modules/ctx096bag/main.lua
@@ -0,0 +1,46 @@
+local MODULE = {
+	name = "SCP 096 Bag",
+	author = "Certurix",
+	version = "1.0.0",
+	description = "A must-have for SCP-096, you can bag him to avoid triggering him!",
+	icon = "icon16/eye.png",
+	version_url = "https://raw.githubusercontent.com/Certurix/vkxscp096/remaster-as-modules-based/lua/guthscp/modules/guthscp096/main.lua",
+	dependencies = {
+		base = "2.0.0",
+		vkxscp096 = "2.0.0",
+	},
+}
+--  config
+MODULE.config = {
+	form = {
+		{
+			type = "Category",
+			name = "General",
+		},
+		{
+			type = "TextEntry",
+			name = "SCP-096 Bag Model",
+			id = "bagmodel",
+			desc = "Define the bag model that will be shown when SCP-096 will be bagged.",
+			default = "models/props_junk/MetalBucket01a.mdl",
+		},
+		guthscp.config.create_apply_button(),
+	},
+	receive = function( form )
+		guthscp.config.apply( MODULE.id, form, {
+			network = true,
+			save = true,
+		} )
+	end,
+}
+
+--  TODO: remove if not used
+function MODULE:construct()
+end
+
+function MODULE:init()
+	print("SCP-096 Bag Module has ben loaded!")
+end
+
+guthscp.module.hot_reload( "ctx096bag" )
+return MODULE
